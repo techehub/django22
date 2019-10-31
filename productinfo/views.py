0f0920aeb54976ec64794747f4b581ad3b9b4ffe
techehub <vijeesh.tp@techehub.com>
2019-10-31 10:38:38 +0530

    Loaded templates

--- productinfo/views.py
+++ productinfo/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import  HttpResponse
+from django.template import loader
 
 
 def productInfo(req):
-    return HttpResponse("This is my product info page ")
+    template=loader.get_template("productinfo.html")
+    data= {}
+    return HttpResponse(template.render(data,req))
