a649d5efbb39eb3c3f35e382bc3080c1f6e749b5
techehub <vijeesh.tp@techehub.com>
2019-11-01 11:24:37 +0530

    Models

--- productinfo/models.py
+++ productinfo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Product (models.Model):
+    pid = models.CharField(max_length=10)
+    pname= models.CharField(max_length=100)
+    desc = models.CharField(max_length=500)
+    price= models.IntegerField()
+    features = models.CharField(max_length=500)
+    mdate= models.DateField()
+
